56bd5d000207579e267ff9159b430b40d4947cee
su.xiaocheng <su.xiaocheng@byd.com>
2023-05-10 15:39:15 +0800

    [bsp][raspberry-pico] add iperf client utils

--- bsp/raspberry-pico/applications/iperf.c
+++ bsp/raspberry-pico/applications/iperf.c
@@ -81,10 +81,95 @@ void iperf_server(void *thread_param)
 	rt_kprintf("%s quit\n", __func__);
 }
 
+void iperf_client(void *thread_param)
+{
+	struct netconn *conn;
+
+	int i;
+
+	int ret;
+
+	uint8_t *send_buf;
+
+	uint64_t sentlen;
+
+	u32_t tick1, tick2;
+	ip4_addr_t ipaddr;
+
+	iperf_need_quit = RT_FALSE;
+
+	send_buf = (uint8_t *) rt_malloc(IPERF_BUFSZ);
+	if (!send_buf)
+		return;
+
+	for (i = 0; i < IPERF_BUFSZ; i++)
+		send_buf[i] = i & 0xff;
+
+	while (1) {
+		conn = netconn_new(NETCONN_TCP);
+		if (iperf_need_quit == RT_TRUE) {
+			break;
+		}
+		if (conn == NULL) {
+			// rt_kprintf("create conn failed!\n");
+			rt_thread_delay(10);
+			continue;
+		}
+
+		IP4_ADDR(&ipaddr, 192, 168, 1, 1);
+
+		ret = netconn_connect(conn, &ipaddr, 5001);
+		if (ret == -1) {
+			rt_kprintf("Connect failed!\n");
+			netconn_close(conn);
+			rt_thread_delay(10);
+			continue;
+		}
+
+		rt_kprintf("Connect to iperf server successful!\n");
+
+		tick1 = sys_jiffies();
+		while (1) {
+			tick2 = sys_jiffies();
+
+			if (tick2 - tick1 >= RT_TICK_PER_SECOND) {
+				float f;
+				int integer, fract;
+				f = (float)(sentlen * RT_TICK_PER_SECOND/ 125 /
+					    (tick2 - tick1));
+				f /= 1000.0f;
+				integer = (int)f;
+				fract = (f-integer)*1000;
+				rt_kprintf("send speed = %d.%d Mbps!\n", integer, fract);
+
+				tick1 = tick2;
+				sentlen = 0;
+			}
+			ret = netconn_write(conn, send_buf, IPERF_BUFSZ, 0);
+			if (ret == ERR_OK) {
+				sentlen += IPERF_BUFSZ;
+			}
+			if (iperf_need_quit == RT_TRUE) {
+				break;
+			}
+		}
+	}
+	if (conn != NULL) {
+		netconn_close(conn);
+		netconn_delete(conn);
+		conn = NULL;
+	}
+
+	if (send_buf) {
+		rt_free(send_buf);
+	}
+}
+
 static void iperf_test(int argc, char **argv)
 {
 	rt_thread_t tid;
 	rt_uint32_t retry = 5;
+	rt_bool_t client = RT_TRUE;
 
 	while (--retry > 0) {
 		tid = rt_thread_find(IPERF_THREAD_NAME);
@@ -102,7 +187,14 @@ static void iperf_test(int argc, char **argv)
 		return;
 	}
 
-	tid = rt_thread_create(IPERF_THREAD_NAME, iperf_server, RT_NULL,
+	if (argc >= 2) {
+		if(rt_strcmp(argv[1], "-s") == 0){
+			client = RT_FALSE;
+		} else {
+			rt_kprintf("unknown option: %s\n", argv[1]);
+		}
+	}
+	tid = rt_thread_create(IPERF_THREAD_NAME, client?iperf_client:iperf_server, RT_NULL,
 			       RT_IPERF_THREAD_STACK_SIZE,
 			       RT_IPERF_THREAD_PRIORITY, 20);
 	RT_ASSERT(tid != RT_NULL);
